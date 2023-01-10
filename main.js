d178a4251b0aa13a9975d1da928fd4487b59c26e
Hero <humphriesa02@gmail.com>
2023-01-09 21:10:22 -0800

    Updated sprite sheet for all 4 directions

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ ASSET_MANAGER.queueDownload("./sprites/pangolin_sheet.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
 
 	let pangolin = new Pangolin(gameEngine);
 	
--- pangolin.js
+++ pangolin.js
@@ -5,9 +5,7 @@ class Pangolin{
 
         // Components
         this.transform = new Transform(new Vec2(0,0), new Vec2(0,0), 1, new Vec2(0,0));
-        this.lifespan;
         this.health;
-        this.input;
         this.collider;
 
         // Reference to our spritesheet
@@ -22,14 +20,13 @@ class Pangolin{
         this.rolling = false;
         this.walk_speed = 0.5;
         this.roll_speed = 3;
-        this.min_walk = 0.1;
-        this.max_walk = 0.5;
 
         // Animations
         this.animations = [];
         this.loadAnimations();
     }
 
+    // Set up our animations variable
     loadAnimations(){
         for (let i = 0; i < 2; i++){ // 2 States right now, idle and walking
         this.animations.push([]);
@@ -41,98 +38,77 @@ class Pangolin{
         // idle animation, state 0
 
         // facing right
-        this.animations[0][0] = new Animator(this.spritesheet, 0, 48, 48, 48, 1, 0.33, true);
+        this.animations[0][0] = new Animator(this.spritesheet, 0, 96, 48, 48, 1, 0.33, true);
 
         // facing left
-        this.animations[0][1] = new Animator(this.spritesheet, 0, 96, 48, 48, 1, 0.33, true);
+        this.animations[0][1] = new Animator(this.spritesheet, 0, 144, 48, 48, 1, 0.33, true);
 
         // facing up
-        this.animations[0][2] = new Animator(this.spritesheet, 0, 144, 48, 48, 1, 0.33, true);
+        this.animations[0][2] = new Animator(this.spritesheet, 0, 48, 48, 48, 1, 0.33, true);
 
         // facing down
         this.animations[0][3] = new Animator(this.spritesheet, 0, 0, 48, 48, 1, 0.33, true);
 
+
         //walking animation, state 1
 
         //facing right
-        this.animations[1][0] = new Animator(this.spritesheet, 0, 48, 48, 48, 2, 0.25, true);
+        this.animations[1][0] = new Animator(this.spritesheet, 0, 96, 48.25, 48, 2, 0.25, true);
 
 
         // facing left
-        this.animations[1][1] = new Animator(this.spritesheet, 0, 96, 48, 48, 2, 0.1, true);
+        this.animations[1][1] = new Animator(this.spritesheet, 0, 144, 48.25, 48, 2, 0.25, true);
 
         // facing up
-        this.animations[1][2] = new Animator(this.spritesheet, 0, 144, 48, 48, 2, 0.1, true);
+        this.animations[1][2] = new Animator(this.spritesheet, 0, 48, 48, 48, 2, 0.25, true);
 
         // facing down
-        this.animations[1][3] = new Animator(this.spritesheet, 0, 0, 48, 48, 2, 0.1, true);
+        this.animations[1][3] = new Animator(this.spritesheet, 0, 0, 48, 48, 2, 0.25, true);
 
 
     }
 
     update(){
-        if(Math.abs(this.transform.velocity.x) < this.min_walk && Math.abs(this.transform.velocity.y) < this.min_walk){ //
+        // Reset velocity
+        this.transform.velocity.x = 0;
+        this.transform.velocity.y = 0;
+
+        // Compute direction from keypresses
+        this.transform.velocity.x = ((-(this.game.keys["a"] ? 1: 0) + (this.game.keys["d"] ? 1: 0)) * this.walk_speed);
+        this.transform.velocity.y = ((-(this.game.keys["w"] ? 1: 0) + (this.game.keys["s"] ? 1: 0)) * this.walk_speed);
+
+        // Figure out the state for animation
+        if (this.transform.velocity.x == 0 && this.transform.velocity.y == 0){
             this.state = 0;
         }
         else{
-            this.state = 1;
-        }
-        console.log(this.state);
-        // When the left key is pressed
-        if(this.game.keys["ArrowLeft"]){ // left
-            if(this.state == 1){ // walking
-                this.transform.velocity.x -= this.walk_speed;
-            } 
-            else{
-                this.transform.velocity.x -= this.roll_speed;
-            }        
+            if(this.rolling){
+                this.state = 2;
+            }
+            else
+                this.state = 1;
         }
 
-        // When the right key is pressed
-        if(this.game.keys["ArrowRight"]){ //Right
-            if(this.state == 1){ // walking
-                this.transform.velocity.x += this.walk_speed;
-            } 
-            else{
-                this.transform.velocity.x += this.roll_speed;
-            }     
-        } 
-
-        // When the down key is pressed
-        if(this.game.keys["ArrowDown"]){ // down
-            if(this.rolling) this.state == 1;
-            else this.state == 2;
-            if(this.state == 1){ // walking
-                this.transform.velocity.y += this.walk_speed;
-            } 
-            else{
-                this.transform.velocity.y += this.roll_speed;
-            }        
+        // Figure out the direction for animation
+        if(this.transform.velocity.x > 0){ // Facing right
+            this.facing = 0;
         }
-
-        // When the up key is pressed
-        if(this.game.keys["ArrowUp"]){ // up
-            if(this.rolling) this.state == 1;
-            else this.state == 2;
-            if(this.state == 1){ // walking
-                this.transform.velocity.y -= this.walk_speed;
-            } 
-            else{
-                this.transform.velocity.y -= this.roll_speed;
-            }        
+        else if (this.transform.velocity.x < 0){ // Facing left
+            this.facing = 1;
         }
-
-
-        if(this.transform.velocity.x >= this.max_walk) this.transform.velocity.x = this.max_walk;
-        if(this.transform.velocity.x <= -this.max_walk) this.transform.velocity.x = -this.max_walk;
-        if(this.transform.velocity.y >= this.max_walk) this.transform.velocity.y = this.max_walk;
-        if(this.transform.velocity.y <= -this.max_walk) this.transform.velocity.y = -this.max_walk;
-
+        if (this.transform.velocity.y < 0){ // Facing up
+            this.facing = 2;
+        }
+        else if (this.transform.velocity.y > 0){ // Facing down
+            this.facing = 3;
+        }
+        
         this.transform.pos.x += this.transform.velocity.x;
         this.transform.pos.y += this.transform.velocity.y;
     }
 
     draw(ctx){
+        console.log("state: ", this.state, " facing: ", this.facing);
         this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.transform.pos.x, this.transform.pos.y)
     }
 
