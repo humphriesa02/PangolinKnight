374e554eafde7ad934a0544d0aff3f03e815bb66
Hero <humphriesa02@gmail.com>
2023-01-17 18:31:49 -0800

    Slashing and jumping

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./sprites/pangolin_sheet.png");
 ASSET_MANAGER.queueDownload("./sprites/pangolin_slash_sheet.png");
 ASSET_MANAGER.queueDownload("./sprites/pangolin_sword_anim.png");
+ASSET_MANAGER.queueDownload("./sprites/pangolin_shadow.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
